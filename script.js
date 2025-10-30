c1f97fbc74dc04504eca0dcf226b8fd3b575c1e6
Giovanna Martins <giovannamartins2604@gmail.com>
2025-10-30 03:35:30 -0300

    atualização

--- script.js
+++ script.js
@@ -28,4 +28,22 @@ const revelarElementos = document.querySelectorAll('.esconder');
 revelarElementos.forEach((el) => observer.observe(el));
 
 
-//toggle contato
+//navbar
+
+(function () {
+    "use strict";  
+  
+    const navbar= document.querySelector(".navbar");
+  
+    document.addEventListener("scroll", (e) => {
+      const scrolled = document.scrollingElement.scrollTop;
+     
+      if (scrolled > 120) { //adjust to suit your need of when transition start
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+  })();
+
+  
